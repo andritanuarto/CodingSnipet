c3d60fde5ea57bb291521c786d17283388968d53
andritanuarto <andritanuarto@gmail.com>
2013-12-01 23:34:44 -0800

    Google Map Modification

--- GoogleMapApiProject/map.js
+++ GoogleMapApiProject/map.js
@@ -1,10 +1,10 @@
 
 var geocoder;
 var map;
+var latlng = new google.maps.LatLng(-34.397, 150.644);
 
 // Initialize Map as first view
 function initialize() {
-  var latlng = new google.maps.LatLng(-34.397, 150.644);
   var mapOptions = {
     zoom: 8,
     center: latlng
@@ -15,25 +15,30 @@ function initialize() {
 //initialize map
 google.maps.event.addDomListener(window, 'load', initialize);
 
-function codeAddress() {
-  geocoder = new google.maps.Geocoder(); //create new geocoder object
+var addresses = document.getElementsByClassName("locationStore");
 
-  var address = document.getElementById('address').value; //get address from value
-  console.log(address);
-  geocoder.geocode(
-      {'address': address}, 
-      function(results, status) {
-        console.log(results);
-        console.log(status);
-        if (status == google.maps.GeocoderStatus.OK) { //check if the address is valid
-          map.setCenter(results[0].geometry.location); //set map center
-          var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location
-          });
-        } else {
-          alert('Geocode was not successful for the following reason: ' + status);
+for(var i = 0; i < addresses.length; i++){
+  addresses[i].onclick = function(){
+    geocoder = new google.maps.Geocoder(); //create new geocoder object
+
+    var address = this.getAttribute("value"); //get address from value
+  
+    geocoder.geocode(
+        {'address': address}, 
+        function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) { //check if the address is valid
+            map.setCenter(results[0].geometry.location); //set map center
+            map.setZoom(15);
+            
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+            });
+
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
         }
-      }
-  );
+    );
+  }
 }
