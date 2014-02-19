1446771240c22a12a5794c40491b3bf9cb29f0e1
andritanuarto <andritanuarto@gmail.com>
2014-02-18 23:31:54 -0800

    Angular 31

--- Angular/31-$routeProvider api/main.js
+++ Angular/31-$routeProvider api/main.js
@@ -1,21 +1,22 @@
 var app = angular.module("app", ["ngRoute"]);
 
 app.config(function($routeProvider) {
-$routeProvider.when('/',
+$routeProvider
+    .when('/',
     {
       templateUrl: "app.html",
       controller: "AppCtrl"
-   	}
-)
+   	})
+    .when('/pizza', {
+      template: "Yum!!!"
+    }).otherwise({
+      template: "This doesn't exist!"
+    })
 });
 
-app.controller("AppCtrl", function($scope, $route) {
-    $route.routes["/"] = {
-        templateUrl: "app.html",
-        controller: "AppCtrl"
-    }
-
+app.controller("AppCtrl", function($scope) {
     $scope.model = {
         message: "This is my app!!!"
     }
 });
+
